c931bff5ae631f3a1732f5420c627df38ab034a6
zhengjie <zhengjie1900@qq.com>
2016-04-04 22:20:48 +0800

    change: clean code

--- app/src/main/java/com/nicaiya/diywidget/DiyWidgetConfigActivity.java
+++ app/src/main/java/com/nicaiya/diywidget/DiyWidgetConfigActivity.java
@@ -3,15 +3,12 @@ package com.nicaiya.diywidget;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 
 import com.nicaiya.diywidget.database.ConfigDataBase;
 import com.nicaiya.diywidget.model.ConfigFileData;
